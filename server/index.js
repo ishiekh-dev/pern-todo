e36746e7e74e2b4a9c4c8d16f250b3a723c59cf0
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-05-06 00:47:30 +0200

    create route for index

--- server/index.js
+++ server/index.js
@@ -15,16 +15,31 @@ app.use(express.json())
 app.post('/todos', async(req, res) => {
     try {
         const { description } = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", [description]);
-        res.json(newTodo)
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING * ", [description]);
+        res.json(newTodo.rows[0])
     } catch (err) {
         console.error(err.message)
     }
 });
 // get all todos 
+app.get('/todos', async(req, res) => {
+    try {
 
+        const allTodos = await pool.query("SELECT * FROM todo ");
+        res.json(allTodos.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 // get a todo 
-
+app.get('/todos/:id', async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo ");
+        res.json(allTodos.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 // update todo 
 
 // delete a todo 
