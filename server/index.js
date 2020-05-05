7c5007a9f2547200ce9b85e07f73b2274b6f8295
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-05-06 00:04:15 +0200

    after connect to db , using pool to insert into todo

--- server/index.js
+++ server/index.js
@@ -8,6 +8,28 @@ const pool = require("./db")
 app.use(cors())
 app.use(express.json())
 
+
+// Routes //
+
+// create todo 
+app.post('/todos', async(req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", [description]);
+        res.json(newTodo)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+// get all todos 
+
+// get a todo 
+
+// update todo 
+
+// delete a todo 
+
+
 app.listen(5000, () => {
     console.log('server has started on port 5000')
 })
