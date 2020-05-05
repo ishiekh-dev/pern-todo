98e165ee66296404a17a6eac941ee099aaf0f636
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-05-06 01:04:45 +0200

    add update route to todo

--- server/index.js
+++ server/index.js
@@ -34,16 +34,34 @@ app.get('/todos', async(req, res) => {
 // get a todo 
 app.get('/todos/:id', async(req, res) => {
     try {
-        const allTodos = await pool.query("SELECT * FROM todo ");
-        res.json(allTodos.rows)
+        const { id } = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1 ", [id]);
+        res.json(todo.rows)
     } catch (err) {
         console.error(err.message)
     }
 });
 // update todo 
-
+app.put('/todos/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        res.send("Todo was Updated");
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 // delete a todo 
-
+app.delete('/todos/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        res.send("Todo was Updated");
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 
 app.listen(5000, () => {
     console.log('server has started on port 5000')
